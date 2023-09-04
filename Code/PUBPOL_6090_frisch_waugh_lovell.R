373eec9992c010836b43a4cf552621444695f56b
theloniousgoerz <theloniouslgoerz@gmail.com>
2023-09-04 18:03:24 -0400

    1. Week 3 Lecture 5 prep

---
+++ Code/PUBPOL_6090_frisch_waugh_lovell.R
@@ -0,0 +1,27 @@
+# Frisch Waugh Lovell by hand 
+# Thelonious Goerz 
+# 9/4/23
+
+set.seed(100)
+
+
+# Create DGP
+x1 = rnorm(100)
+x2 = rnorm(100)
+y1 = 1 + x1 - x2 + rnorm(100)
+
+# Target: b1
+
+# 1. Regress y on x2
+r1 = residuals(lm(y1 ~ x2))
+# 2. Regress x1 on x2
+r2 = residuals(lm(x1 ~ x2))
+
+# ols
+coef(lm(y1 ~ x1 + x2))
+
+
+# fwl ols
+# Remove intercet
+plot(fitted(lm(r1 ~ -1 + r2)))
+
